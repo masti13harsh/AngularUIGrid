6020fdb3d9b2355f36cc33da3164730f1b5cc480
masti13harsh <harshsapra@Harshs-MacBook-Pro.local>
2016-05-03 16:52:10 +0530

    First Commit

--- js/app/app.js
+++ js/app/app.js
@@ -1,41 +1,38 @@
-var myApp = angular.module('myApp', ["jqwidgets"]);
+var myApp = angular.module('myApp', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.exporter']);
 
 myApp.controller("demoController", function ($scope, $http) {
-    $scope.createWidget = false;
-    $http({
-        method: 'get',
-        url: 'data/beverages.txt'
-    }).success(function (data, status) {
-        // prepare the data
-        var source =
-        {
-            datatype: "json",
-            datafields: [
-                { name: 'name', type: 'string' },
-                { name: 'type', type: 'string' },
-                { name: 'calories', type: 'int' },
-                { name: 'totalfat', type: 'string' },
-                { name: 'protein', type: 'string' }
-            ],
-            id: 'id',
-            localdata: data
-        };
-        var dataAdapter = new $.jqx.dataAdapter(source);
-        $scope.gridSettings =
-        {
-            width: 850,
-            source: dataAdapter,
-            columnsresize: true,
-            columns: [
-              { text: 'Name', datafield: 'name', width: 250 },
-              { text: 'Beverage Type', datafield: 'type', width: 250 },
-              { text: 'Calories', datafield: 'calories', width: 180 },
-              { text: 'Total Fat', datafield: 'totalfat', width: 120 },
-              { text: 'Protein', datafield: 'protein', minwidth: 120 }
-            ]
-        };
-        $scope.createWidget = true;
-    }).error(function (data, status) {
-        // Some error occurred
-    });
+  $scope.gridOptions = {
+  columnDefs: [
+    { field: 'name' },
+    { field: 'gender', visible: false},
+    { field: 'company' }
+  ],
+  enableGridMenu: true,
+  enableSelectAll: true,
+  exporterCsvFilename: 'myFile.csv',
+  exporterPdfDefaultStyle: {fontSize: 9},
+  exporterPdfTableStyle: {margin: [30, 30, 30, 30]},
+  exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, italics: true, color: 'red'},
+  exporterPdfHeader: { text: "My Header", style: 'headerStyle' },
+  exporterPdfFooter: function ( currentPage, pageCount ) {
+    return { text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle' };
+  },
+  exporterPdfCustomFormatter: function ( docDefinition ) {
+    docDefinition.styles.headerStyle = { fontSize: 22, bold: true };
+    docDefinition.styles.footerStyle = { fontSize: 10, bold: true };
+    return docDefinition;
+  },
+  exporterPdfOrientation: 'portrait',
+  exporterPdfPageSize: 'LETTER',
+  exporterPdfMaxGridWidth: 500,
+  exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+  onRegisterApi: function(gridApi){
+    $scope.gridApi = gridApi;
+  }
+};
+
+$http.get('data/data.json')
+.success(function(data) {
+  $scope.gridOptions.data = data;
+});
 });
